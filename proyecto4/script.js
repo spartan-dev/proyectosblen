c3d3d03e5e57d73598dc1b82b4f9652566132e22
salemm <s.e.jugr@gmail.com>
2020-11-12 19:42:58 -0600

    tarea agregada y respuesta para las notas :skull:

--- proyecto4/script.js
+++ proyecto4/script.js
@@ -1,33 +1,40 @@
-/* escribir los elementos  */
-//notesEl editBtn deleteBtn
-// agregar el main de noteEl con query selector
-// agregar el text arra con el noteEl de query selector de text
-//escuchador del editbtn  agregar la clase de hiden y text
-// en el escuchador main class lsit y text area
-// text area de el event listener  y dentro su main inner y su marked value
-// agregar button de add
+//! le da funcon al boton para tomarlo del dom
 const addBtn = document.getElementById("add");
 
-addBtn.addEventListener("click", () => {
-  //este codigo relaciona a el boton con su trabajo click
+const notes = JSON.parse(localStorage.getItem("notes"));
+//! ocupamos un if para ver si las notas estan o existen
+if (notes) {
+  //! checar por booleanos
 
+  /*  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+  } */
+  notes.forEach((note) => {
+    addNewNote(note);
+  });
+}
+
+addBtn.addEventListener("click", () => {
+  //llama a la funcion de addew note
   addNewNote();
 });
 
 function addNewNote(text = "") {
-  console.log("sillegamos aca ?? 🤕");
+  //
   const note = document.createElement("div");
   note.classList.add("note");
+
   note.innerHTML = `
-   <div class="note">
-   <div class="tools">
-   <button class="edit"><i class="fas fa-edit"></i></button>
-   <button class="delete"><i class="fas fa-trash-alt"></i></button>
-      </div>
-      <div class="main ${text ? "" : "hidden"}"></div>
-        <textarea class="${text ? "hidden" : ""}"></textarea>
-   </div>
-  `;
+        <div class="notes">
+            <div class="tools">
+                <button class="edit"><i class="fas fa-edit"></i></button>
+                <button class="delete"><i class="fas fa-trash-alt"></i></button>
+            </div>
+            <div class="main ${text ? "" : "hidden"}"></div>
+            <textarea class="${text ? "hidden" : ""}"></textarea>
+        </div>
+    `;
+
   const editBtn = note.querySelector(".edit");
   const deleteBtn = note.querySelector(".delete");
 
@@ -35,24 +42,38 @@ function addNewNote(text = "") {
   const textArea = note.querySelector("textarea");
 
   textArea.value = text;
-
-  deleteBtn.addEventListener("click", () => {
-    note.remove();
-  });
+  main.innerHTML = marked(text);
 
   editBtn.addEventListener("click", () => {
     main.classList.toggle("hidden");
-    textArea.classList("hidden");
+    textArea.classList.toggle("hidden");
+  });
+
+  deleteBtn.addEventListener("click", () => {
+    note.remove();
+
+    updateLS();
+  });
+
+  textArea.addEventListener("input", (e) => {
+    const { value } = e.target;
+
+    main.innerHTML = marked(value);
+
+    updateLS();
   });
 
   document.body.appendChild(note);
 }
 
-function updateLs() {
-  const noteText = document.querySelectorAll("textarea");
+function updateLS() {
+  const notesText = document.querySelectorAll("textarea");
+
   const notes = [];
-  noteText.forEach((note) => {
+
+  notesText.forEach((note) => {
     notes.push(note.value);
   });
-  localStorage.setItem("note", JSON.stringify(notes));
+
+  localStorage.setItem("notes", JSON.stringify(notes));
 }
